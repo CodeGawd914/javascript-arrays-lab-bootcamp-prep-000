1d9fbb9b62db1b1d8df6eca04a5c1aa430de790a
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-07-27 19:42:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,10 @@ function destructivelyRemoveFirstKitten(){
 function appendKitten(name){
   return  [...kittens, name]
 }
-function prependKittens
+function prependKitten(name){
+  return [name,...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(-1)
+}
+function removeFirstKitten
