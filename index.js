ce8123fab46900fa993a66bdee9738278b3a4e04
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-07-27 19:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+ return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+function appendKitten(name){
+  [...kittens, "Broom"]
+  return kittens 
+}
